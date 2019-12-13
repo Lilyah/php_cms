4f8f52107a125d6d376a44371d059fb38f757b2e
Lilyah <lilyah.boz@gmail.com>
2019-12-13 14:24:44 +0200

    fixing heroku db credentials

--- includes/db.php
+++ includes/db.php
@@ -10,23 +10,21 @@ $db['db_host'] = "localhost";
 /* For more secure:
  * Collecting connection data into array and converting it to constants, uppercase.
 */
-$db = [
-    "db_host" => 'localhost',
-    "db_user" => 'root',
-    "db_pass" => '',
-    "db_name" => 'cms'
-];
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+/* Actual connection HERE */
+$connection = new mysqli($server, $username, $password, $db);
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
 }
-
-/* Actual connection HERE */
-$connection = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if(!$connection) {
     die("Database connection failed.");
 }
-
-
 
 ?>
