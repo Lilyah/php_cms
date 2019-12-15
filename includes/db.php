d2397174e6c2584ae8aa2c6641fc89391882c8d0
Lilyah <lilyah.boz@gmail.com>
2019-12-15 09:39:20 +0200

    Checking if con is local or global and set different host, user, pass, name

--- includes/db.php
+++ includes/db.php
@@ -1,30 +1,49 @@
 <?php
 
-/*
-$db['db_host'] = "localhost";
-$db['db_host'] = "localhost";
-$db['db_host'] = "localhost";
-$db['db_host'] = "localhost";
-*/
-
 /* For more secure:
  * Collecting connection data into array and converting it to constants, uppercase.
 */
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+/* For LOCAL CON */
+$db = [
+    "db_host" => 'localhost',
+    "db_user" => 'root',
+    "db_pass" => '',
+    "db_name" => 'cms'
+];
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-/* Actual connection HERE */
-$connection = new mysqli($server, $username, $password, $db);
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
 }
+
+/* Actual connection HERE */
+$connection = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if(!$connection) {
-    die("Database connection failed.");
+//    die("Database connection failed.");
+//}
+
+////////////////////
+
+    /* For more secure:
+     * Collecting connection data into array and converting it to constants, uppercase.
+    */
+
+    /* For GLOBAL CON */
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    /* Actual connection HERE */
+    $connection = new mysqli($server, $username, $password, $db);
+    foreach ($db as $key => $value) {
+        define(strtoupper($key), $value);
+    }
+    if (!$connection) {
+        die("Database connection failed.");
+    }
 }
 
 ?>
